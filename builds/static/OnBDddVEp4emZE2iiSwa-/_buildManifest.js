a260e320f51ee73496a684ef83c328135e97ae13
leocheuk <leocheuk@vgt-hk.net>
2020-08-14 14:20:23 +0800

    delte build folder

--- builds/static/OnBDddVEp4emZE2iiSwa-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002F624b4783ef4103dced34.css","static\u002Fchunks\u002Fpages\u002Findex-8727074ba7913dcd7447.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
